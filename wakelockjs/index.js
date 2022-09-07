aa6a519ccf42fe7d5a7c6f67b31877c418ff97fc
Duskamo <dyanownz@yahoo.com>
2022-09-06 19:39:55 -0500

    transfer wakelock ex

---
+++ wakelockjs/index.js
@@ -0,0 +1,36 @@
+
+// Enabling lock
+async function getScreenLock() {
+  if(isScreenLockSupported()){
+    let screenLock;
+    try {
+       screenLock = await navigator.wakeLock.request('screen');
+    } catch(err) {
+       console.log(err.name, err.message);
+    }
+    return screenLock;
+  }
+
+  // Function calls	
+  let screenLock = await getScreenLock();
+  //release();	
+}
+
+// Disabiling lock
+function release() { 
+  if(typeof screenLock !== "undefined" && screenLock != null) {
+    screenLock.release()
+    .then(() => {
+      console.log("Lock released 🎈");
+      screenLock = null;
+    });
+  }
+}
+
+function isScreenLockSupported() {
+ return ('wakeLock' in navigator);
+}
+
+getScreenLock();
+
+
