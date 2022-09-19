191c5a46c5fa6868aac728a9fc01853ed23ab6fb
Duskamo <dyanownz@yahoo.com>
2022-09-18 20:08:48 -0500

    updated links

--- ACDS/advancedautoclicker.py
+++ ACDS/advancedautoclicker.py
@@ -9,5 +9,9 @@ app = Flask(__name__,
 def index():
     return render_template('index.html')
 
+@app.route('/download')
+def download():
+    return render_template('download.html')
+
 if __name__ == '__main__':
     app.run()
